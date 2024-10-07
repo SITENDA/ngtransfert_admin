5c08304f2fed9dc2e3cccc41fb6dcbc399477feb
SITENDA <amossitenda@gmail.com>
2024-10-07 12:15:32 +0800

    Tried to improve the two way exchange.

--- src/features/topUp/TopUp.js
+++ src/features/topUp/TopUp.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {Typography, Button, Grid, Card, CardActions, CardContent, Divider} from '@mui/material';
+import {Typography, Button, Grid, Card, CardActions, CardContent, Divider, CircularProgress, Box} from '@mui/material';
 import {darkColor, lightColor} from "../../util/initials";
 import ImageDisplay from "../../components/form-controls/ImageDisplay";
 import TickAnimation from "../../components/TickAnimation";
@@ -24,26 +24,32 @@ const TopUp = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const location = useLocation();
-    const isDarkTheme = useSelector(selectIsDarkTheme);
-    const [tickAnimationVisible, setTickAnimationVisible] = useState(false);
-    const [directionOfExchange, setDirectionOfExchange] = useState("forward")
 
     const alipayAccount = location?.state?.alipayAccount;
     const wechatAccount = location?.state?.wechatAccount;
     const bankAccount = location?.state?.bankAccount;
+
+    const isDarkTheme = useSelector(selectIsDarkTheme);
+    const topUpInputs = useSelector(selectTopUpInputs)
+    const validTopUp = useSelector(selectValidTopUp)
+    const topUpFocus = useSelector(selectTopUpFocus)
+
+    const [tickAnimationVisible, setTickAnimationVisible] = useState(false);
+    const [directionOfExchange, setDirectionOfExchange] = useState("forward");
+    const [canSubmit, setCanSubmit] = useState(false);
+    const [isTypingForward, setIsTypingForward] = useState(false);
+    const [isTypingBackward, setIsTypingBackward] = useState(false);
+    const [countryOfDeposit, setCountryOfDeposit] = useState(null);
+    const [sourceCurrencyCode, setSourceCurrencyCode] = useState(null);
+    const [exchangeData, setExchangeData] = useState(null);
+
     const stateSelectedMethod = location.state.selectedMethod;
     const foundMethod = orderedTopUpMethods.find(method => method.value === stateSelectedMethod);
     const selectedMethod = {...foundMethod, label: <SelectedMethodDisplay method={foundMethod}/>};
     const selectedCountryName = location.state.selectedCountryName;
 
-    const topUpInputs = useSelector(selectTopUpInputs)
-    const validTopUp = useSelector(selectValidTopUp)
-    const topUpFocus = useSelector(selectTopUpFocus)
-
     const {data: countryData, isSuccess: countryFetched} = useGetCountryByCountryNameQuery(selectedCountryName);
-    const [countryOfDeposit, setCountryOfDeposit] = useState(null);
-    const [sourceCurrencyCode, setSourceCurrencyCode] = useState(null);
-    const [exchangeData, setExchangeData] = useState(null);
+
 
     const [doCurrencyExchange, {
         data: currencyExchangeData,
@@ -53,12 +59,23 @@ const TopUp = () => {
     }] = useDoCurrencyExchangeMutation();
 
     useEffect(() => {
+        setCanSubmit(false);
+        setIsTypingForward(false);
+        setIsTypingBackward(false);
+    }, [isSuccess, isError]);
+
+    useEffect(() => {
         if (countryFetched && countryData?.data?.country) {
             setCountryOfDeposit(countryData?.data?.country)
             setSourceCurrencyCode(countryData?.data?.country?.currency?.currencyCode)
-            dispatch(setObjectItem({key: "topUpInputs", innerKey: "sourceCurrencyCode", value: countryData?.data?.country?.currency?.currencyCode}))
+            dispatch(setObjectItem({
+                key: "topUpInputs",
+                innerKey: "sourceCurrencyCode",
+                value: countryData?.data?.country?.currency?.currencyCode
+            }))
         }
     }, [countryData, countryFetched, countryOfDeposit, dispatch]);
+
 
     const refs = {
         topUpMethodRef: useRef(null),
@@ -71,7 +88,7 @@ const TopUp = () => {
         dispatch(setItem({key: "title", value: "Top Up"}));
     }, [dispatch]);
 
-    useEffect(() => {
+    const handleChange = () => {
         const handleCurrencyExchange = async () => {
             if ((isValidNumber(topUpInputs.sourceAmount) || isValidNumber(topUpInputs.targetAmount)) &&
                 isValidNumber(countryOfDeposit?.countryId) &&
@@ -81,32 +98,75 @@ const TopUp = () => {
                 try {
                     // Trigger the mutation with the appropriate arguments
                     await doCurrencyExchange({
-                        sourceCurrencyCode : topUpInputs.sourceCurrencyCode,
-                        targetCurrencyCode : topUpInputs.targetCurrencyCode,
+                        sourceCurrencyCode: topUpInputs.sourceCurrencyCode,
+                        targetCurrencyCode: topUpInputs.targetCurrencyCode,
                         sourceAmount: topUpInputs.sourceAmount || 0,
                         targetAmount: topUpInputs.targetAmount || 0,
                         countryId: countryOfDeposit?.countryId || 1,  // Fallback for testing
                         directionOfExchange: directionOfExchange,
                     }).unwrap();
+
+                    // Dispatch updated amounts based on direction
                     if (directionOfExchange === "forward") {
-                        dispatch(setObjectItem({key: "topUpInputs", innerKey: "targetAmount", value: currencyExchangeData?.targetAmount}))
+                        dispatch(setObjectItem({
+                            key: "topUpInputs",
+                            innerKey: "targetAmount",
+                            value: currencyExchangeData?.targetAmount
+                        }));
+                    } else {
+                        dispatch(setObjectItem({
+                            key: "topUpInputs",
+                            innerKey: "sourceAmount",
+                            value: currencyExchangeData?.sourceAmount
+                        }));
                     }
-                    else {
-                        dispatch(setObjectItem({key: "topUpInputs", innerKey: "sourceAmount", value: currencyExchangeData?.sourceAmount}))
-                    }
-                    setExchangeData(currencyExchangeData)
+                    setExchangeData(currencyExchangeData);
 
                 } catch (exception) {
                     console.log("Error is: ", exception);
                 }
-
             } else {
                 console.log("One of the inputs is still invalid");
             }
         };
+        // Start a timer to reset isTyping to true after 3 seconds
+        const timer = setTimeout(() => {
+            setCanSubmit(true); // Set isTyping to false
+            console.log("Typing forward : ", isTypingForward)
+            console.log("Typing backward : ", isTypingBackward)
+            if (isTypingForward || isTypingBackward) {
+                handleCurrencyExchange(); // Trigger the exchange only after isTyping is true
+            }
+            setCanSubmit(false);
+        }, 3000); // 3-second debounce delay
 
-        handleCurrencyExchange();
-    }, [countryOfDeposit?.countryId, topUpInputs.sourceCurrencyCode, topUpInputs.targetCurrencyCode, dispatch, doCurrencyExchange, sourceCurrencyCode, topUpInputs.sourceAmount, topUpInputs.targetAmount]);
+        // Clear the timeout if isTyping changes again within the delay
+        return () => clearTimeout(timer);
+    }
+
+    useEffect(handleChange, [
+        canSubmit,
+        countryOfDeposit?.countryId,
+        topUpInputs.sourceCurrencyCode,
+        topUpInputs.targetCurrencyCode,
+        dispatch,
+        doCurrencyExchange,
+        sourceCurrencyCode,
+        topUpInputs.sourceAmount,
+        directionOfExchange,
+    ]);
+
+    useEffect(handleChange, [
+        canSubmit,
+        countryOfDeposit?.countryId,
+        topUpInputs.sourceCurrencyCode,
+        topUpInputs.targetCurrencyCode,
+        dispatch,
+        doCurrencyExchange,
+        sourceCurrencyCode,
+        topUpInputs.targetAmount,
+        directionOfExchange,
+    ]);
 
 
     const handleConfirmTopUp = () => {
@@ -168,60 +228,96 @@ const TopUp = () => {
 
                                     {validTopUp.validCountryOfTopUpId && validTopUp.validTopUpMethod &&
                                         <>
-                                            <AmountInput
-                                            ref={refs.sourceAmountRef}
-                                            changeHandler={(e) => {
-                                                setDirectionOfExchange("forward")
-                                                if (e.target.value === 0) {dispatch(setObjectItem({key: "topUpInputs", innerKey: "targetAmount", value: 0}))}
-                                                return dispatch(handleValidation({
-                                                objectName: "topUp",
-                                                eventValue: e.target.value,
-                                                inputName: "sourceAmount",
-                                                regexPattern: "AMOUNT_REGEX"
-                                            }))
-                                            }}
-                                            validAmount={validTopUp.validSourceAmount}
-                                            value={topUpInputs.sourceAmount}
-                                            label={countryOfDeposit?.currency?.currencyCode ? `Amount in ${countryOfDeposit?.currency?.currencyCode}` : "Amount"}
-                                            isFocused={topUpFocus.sourceAmountFocus}
-                                            handleFocus={() => dispatch(handleFocus({
-                                                objectName: 'topUp',
-                                                inputName: "sourceAmount"
-                                            }))}
-                                            handleBlur={() => dispatch(handleBlur({
-                                                objectName: 'topUp',
-                                                inputName: "sourceAmount",
-                                                regexPattern: "AMOUNT_REGEX"
-                                            }))}/>
 
-                                        <AmountInput
-                                            ref={refs.targetAmountRef}
-                                            changeHandler={(e) => {
-                                                setDirectionOfExchange("backward")
-                                                if (e.target.value === 0) {dispatch(setObjectItem({key: "topUpInputs", innerKey: "sourceAmount", value: 0}))}
-                                                return dispatch(handleValidation({
-                                                objectName: "topUp",
-                                                eventValue: e.target.value,
-                                                inputName: "targetAmount",
-                                                regexPattern: "AMOUNT_REGEX"
-                                            }))
-                                            }}
-                                            validAmount={validTopUp.validTargetAmount}
-                                            value={topUpInputs.targetAmount}
-                                            label="Amount in CNY"
-                                            isFocused={topUpFocus.targetAmountFocus}
-                                            handleFocus={() => {
-                                                console.log("targetAmount focused");
-                                                return dispatch(handleFocus({
-                                                objectName: 'topUp',
-                                                inputName: "targetAmount"
-                                            }))}
-                                            }
-                                            handleBlur={() => dispatch(handleBlur({
-                                                objectName: 'topUp',
-                                                inputName: "targetAmount",
-                                                regexPattern: "AMOUNT_REGEX"
-                                            }))}/>
+                                            {(directionOfExchange === "backward") && (isLoading || isTypingBackward) ? (
+                                                <CircularProgress size={15}/>
+                                            ) : (<AmountInput
+                                                ref={refs.sourceAmountRef}
+                                                changeHandler={(e) => {
+                                                    setCanSubmit(false)
+                                                    setDirectionOfExchange("forward")
+                                                    setIsTypingForward(true)
+                                                    if (e.target.value === 0) {
+                                                        dispatch(setObjectItem({
+                                                            key: "topUpInputs",
+                                                            innerKey: "targetAmount",
+                                                            value: 0
+                                                        }))
+                                                    }
+                                                    dispatch(handleValidation({
+                                                        objectName: "topUp",
+                                                        eventValue: e.target.value,
+                                                        inputName: "sourceAmount",
+                                                        regexPattern: "AMOUNT_REGEX"
+                                                    }))
+                                                    return dispatch(handleValidation({
+                                                        objectName: "topUp",
+                                                        eventValue: topUpInputs.targetAmount,
+                                                        inputName: "targetAmount",
+                                                        regexPattern: "AMOUNT_REGEX"
+                                                    }))
+                                                }}
+                                                validAmount={validTopUp.validSourceAmount}
+                                                value={topUpInputs.sourceAmount}
+                                                label={countryOfDeposit?.currency?.currencyCode ? `Amount in ${countryOfDeposit?.currency?.currencyCode}` : "Amount"}
+                                                isFocused={topUpFocus.sourceAmountFocus}
+                                                handleFocus={() => dispatch(handleFocus({
+                                                    objectName: 'topUp',
+                                                    inputName: "sourceAmount"
+                                                }))}
+                                                handleBlur={() => dispatch(handleBlur({
+                                                    objectName: 'topUp',
+                                                    inputName: "sourceAmount",
+                                                    regexPattern: "AMOUNT_REGEX"
+                                                }))}/>)}
+
+
+                                            {(directionOfExchange === "forward") && (isLoading || isTypingForward) ? (
+                                                <CircularProgress size={15}/>
+                                            ) : (<AmountInput
+                                                ref={refs.targetAmountRef}
+                                                changeHandler={(e) => {
+                                                    setCanSubmit(false)
+                                                    setDirectionOfExchange("backward")
+                                                    setIsTypingBackward(true)
+                                                    if (e.target.value === 0) {
+                                                        dispatch(setObjectItem({
+                                                            key: "topUpInputs",
+                                                            innerKey: "sourceAmount",
+                                                            value: 0
+                                                        }))
+                                                    }
+
+                                                    dispatch(handleValidation({
+                                                        objectName: "topUp",
+                                                        eventValue: e.target.value,
+                                                        inputName: "targetAmount",
+                                                        regexPattern: "AMOUNT_REGEX"
+                                                    }));
+                                                    return dispatch(handleValidation({
+                                                        objectName: "topUp",
+                                                        eventValue: topUpInputs.sourceAmount,
+                                                        inputName: "sourceAmount",
+                                                        regexPattern: "AMOUNT_REGEX"
+                                                    }))
+                                                }}
+                                                validAmount={validTopUp.validTargetAmount}
+                                                value={topUpInputs.targetAmount}
+                                                label="Amount in CNY"
+                                                isFocused={topUpFocus.targetAmountFocus}
+                                                handleFocus={() => {
+                                                    return dispatch(handleFocus({
+                                                        objectName: 'topUp',
+                                                        inputName: "targetAmount"
+                                                    }))
+                                                }
+                                                }
+                                                handleBlur={() => dispatch(handleBlur({
+                                                    objectName: 'topUp',
+                                                    inputName: "targetAmount",
+                                                    regexPattern: "AMOUNT_REGEX"
+                                                }))}/>)}
+
                                         </>
 
 
